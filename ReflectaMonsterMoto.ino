f5592c052636ca5ee4887d9f67248370e7c1f857
Jay Beavers <jay@hikinghomeschoolers.org>
2012-09-21 16:45:15 -0700

    Documentation refresh

--- ReflectaMonsterMoto.ino
+++ ReflectaMonsterMoto.ino
@@ -8,12 +8,15 @@ void adjustPwmFrequency()
   // Adjust the timer prescaler to move the motor pwm frequency outside of human hearing
   // AKA get rid of the whine.
   
-  TCCR0B &= ~0x07; // Set the lower three bits (prescaler value) to 000 to remove previous setting (generally 011)
-  TCCR0B |= 0x01;  // Set the lower three bits to 001
+  int prescaler = 0x07;
+  TCCR0B &= ~prescaler; // Set the lower three bits (prescaler value) to 000 to remove previous setting (generally 011)
+  prescaler = 0x01;
+  TCCR1B |= prescaler;  // Set the lower three bits to 001
 }
 
 void setup()
 {
+  adjustPwmFrequency();
   reflecta::setup(57200);
   monsterMotoSetup();
 }
